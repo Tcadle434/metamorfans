588e6b645790843aa6e65ea790cfa1f46816f4fa
Tcadle434 <thomascadle434@gmail.com>
2022-02-21 10:54:51 -0700

    https

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
+  <HttpsRedirect>
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </HttpsRedirect>,
   document.getElementById('root')
 );
 
